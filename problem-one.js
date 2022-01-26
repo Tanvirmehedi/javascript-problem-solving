907ee072b1d208cdbf82cd1409b257ae4a23ca86
Tanvirmehedi <tjmehedi201@gmail.com>
2022-01-27 02:42:11 +0600

    Problem 1st is solved but 2nd still not

---
+++ problem-one.js
@@ -0,0 +1,125 @@
+// Practice Challenge-1:
+
+// You are first in the final exam of class saver. Your friends Alia, Dahlia, Salia, Malia, Lilia and Jbaliya - you don't know their grade. But know their number. Alia got 95, Dahlia got 66, Salia got 80, Malia got 59, Lilia got 47, Jbaliya got 77. Can you figure out their grade with JS code?
+
+// 1) Those who got below 50, their grade F.
+
+// 2) Those who got 50 or above, or got below 60, their grade D.
+
+// 3) Those who got 60 or above, or got below 70, their grade C.
+
+// 4) Those who got 60 or above, or got below 80, their grade B.
+
+// 5) Those who got 80 or above, or got below 90, their grade A.
+
+// 6) Those who got 90 or above, their grade A +.
+
+// VARIABLES FOR INDIVIDUAL
+var alia = 95;
+var dahlia = 66;
+var salia = 80;
+var malia = 59;
+var lilia = 47;
+var jbaliya = 77;
+
+if (alia > 50) {
+  if (alia >= 50 && alia < 60) {
+    console.log("Alia Pass The exam and Her grade is D");
+  } else if (alia >= 60 && alia < 70) {
+    console.log("Alia Pass The Exam And Her grade is C");
+  } else if (alia >= 70 && alia < 80) {
+    console.log("Alia Pass The Exam And Her grade is B");
+  } else if (alia >= 80 && alia < 90) {
+    console.log("Alia Pass The Exam And Her grade is A");
+  } else if (alia >= 90) {
+    console.log("Alia Pass The Exam And Her grade is A+");
+  }
+} else {
+  console.log("Alias grade Point is F");
+}
+
+// END OF ALIAS RESULT
+
+if (salia > 50) {
+  if (salia >= 50 && salia < 60) {
+    console.log("Salia Pass The exam and Her grade is D");
+  } else if (salia >= 60 && salia < 70) {
+    console.log("Salia Pass The Exam And Her grade is C");
+  } else if (salia >= 70 && salia < 80) {
+    console.log("Salia Pass The Exam And Her grade is B");
+  } else if (salia >= 80 && salia < 90) {
+    console.log("Salia Pass The Exam And Her grade is A");
+  } else if (salia >= 90) {
+    console.log("Salia Pass The Exam And Her grade is A+");
+  }
+} else {
+  console.log("Salia grade Point is F");
+}
+
+// END OF Salia RESULT
+
+if (malia > 50) {
+  if (malia >= 50 && malia < 60) {
+    console.log("Malia Pass The exam and Her grade is D");
+  } else if (malia >= 60 && malia < 70) {
+    console.log("Malia Pass The Exam And Her grade is C");
+  } else if (malia >= 70 && malia < 80) {
+    console.log("Malia Pass The Exam And Her grade is B");
+  } else if (malia >= 80 && malia < 90) {
+    console.log("Malia Pass The Exam And Her grade is A");
+  } else if (malia >= 90) {
+    console.log("Malia Pass The Exam And Her grade is A+");
+  }
+} else {
+  console.log("Malia grade Point is F");
+}
+
+// END OF MALIA RESULT
+
+if (jbaliya >= 50 && jbaliya < 60) {
+  console.log("Jbaliya Pass The exam and Her grade is D");
+} else if (jbaliya >= 60 && jbaliya < 70) {
+  console.log("Jbaliya Pass The Exam And Her grade is C");
+} else if (jbaliya >= 70 && jbaliya < 80) {
+  console.log("Jbaliya Pass The Exam And Her grade is B");
+} else if (jbaliya >= 80 && jbaliya < 90) {
+  console.log("Jbaliya Pass The Exam And Her grade is A");
+} else if (jbaliya >= 90) {
+  console.log("Jbaliya Pass The Exam And Her grade is A+");
+} else {
+  console.log("Jbaliya grade Point is F");
+}
+
+// END OF Jbaliya RESULT
+
+if (lilia >= 50 && lilia < 60) {
+  console.log("Lilia Pass The exam and Her grade is D");
+} else if (lilia >= 60 && lilia < 70) {
+  console.log("Lilia Pass The Exam And Her grade is C");
+} else if (lilia >= 70 && lilia < 80) {
+  console.log("Lilia Pass The Exam And Her grade is B");
+} else if (lilia >= 80 && lilia < 90) {
+  console.log("Lilia Pass The Exam And Her grade is A");
+} else if (lilia >= 90) {
+  console.log("Lilia Pass The Exam And Her grade is A+");
+} else {
+  console.log("Lilia grade Point is F");
+}
+
+// END OF Lilia RESULT
+
+if (dahlia >= 50 && dahlia < 60) {
+  console.log("Dahlia Pass The exam and Her grade is D");
+} else if (dahlia >= 60 && dahlia < 70) {
+  console.log("Dahlia Pass The Exam And Her grade is C");
+} else if (dahlia >= 70 && dahlia < 80) {
+  console.log("Dahlia Pass The Exam And Her grade is B");
+} else if (dahlia >= 80 && dahlia < 90) {
+  console.log("Dahlia Pass The Exam And Her grade is A");
+} else if (dahlia >= 90) {
+  console.log("Dahlia Pass The Exam And Her grade is A+");
+} else {
+  console.log("Dahlia grade Point is F");
+}
+
+// END OF Dahlia RESULT
---
+++ problem-tow.js
@@ -0,0 +1,3 @@
+// Practice Problem-2:
+
+// Getting out of school in a hurry, but when crossing the road, the traffic signal is red. In this case, if you try to cross the road, it can be dangerous. If it is yellow then you should stop. And if the traffic signal is green then you should cross the road. So write a code. Where we have a variable called signal. Its value can be green, yellow or red. Different work will be done accordingly. So, write that code quickly.
